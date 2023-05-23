588f1e51f25f320eb8e9a1672bf6d659299e70d5
pascaloseko <pascal.ondieki21@gmail.com>
2023-05-23 17:10:11 +0300

    apply other configs

--- src/lib/firebase.config.js
+++ src/lib/firebase.config.js
@@ -4,10 +4,10 @@ import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "pasco-1345.firebaseapp.com",
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DB_URL,
-  projectId: "pasco-1345",
-  storageBucket: "pasco-1345.appspot.com",
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID
